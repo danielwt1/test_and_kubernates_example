ee35e7aa1f9b26e59f0ca072c423378d6b0ae229
Daniel Ramos <dramosg@ucentral.edu.co>
2023-09-10 16:17:17 -0500

    Se añaden archivos de pruebas para angular, y config de Kubernates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+      // cada it es un testeo 
+    //esta beforeeach se ejecuta antesd de cada test y aftereach despues
   beforeEach(async () => {
+    // tengo que ikmportar y preparar aca lo que usa en modulos ese componente o si no falla 
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
+  it('El componente esta creado correctamente! ', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'docker_example_angular'`, () => {
+  //prueba para ver que el formulario esta invalido 
+  it(' cuando se agrega usuario pero no clave  entonces es invalido',()=>{
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('docker_example_angular');
+    fixture.detectChanges();
+    const num1 = app.form.controls['numero1'];
+    num1.setValue('2');
+    expect(app.form.invalid).toBe(true);
   });
+  it('Debe retoprnar true ',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    fixture.detectChanges();
+    const num1 = app.form.controls['numero1'];
+    const num2 = app.form.controls['numero2'];
+    num1.setValue('2');
+    num2.setValue('4');
+    // referencia a un elemento referizado si tengo un html con clase puedo usar por ej  button.cssclass
+    const btnElement = fixture.debugElement.query(By.css('button'));
+    btnElement.nativeElement.click();
+    expect(app.result).toEqual(6);
+  });
+
+
+    // test it('nombretest',)
+    it('whenCallSumFuctionThenReturnfour',waitForAsync(()=>{
+      // condicion de exito
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      //(<HTMLInputElement>)
+    }));
+
+/*
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -32,4 +64,5 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('docker_example_angular app is running!');
   });
+  */
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'docker_example_angular';
+  result:number=0;
+  form =   this._fb.group({
+    numero1:['',[Validators.required]],
+    numero2:['',[Validators.required]]
+  }); 
+  constructor (private _fb:FormBuilder){
+
+  }
+  ngOnInit(): void {
+  }
+
+  
+  public suma(){
+    const num1: number = parseFloat(this.form.controls.numero1.value!);
+    const num2: number = parseFloat(this.form.controls.numero2.value!);
+    console.log(num1, num2)  
+    this.result = num1+num2;
+  
+    return this.result;
+  }
+
+
+
 }
